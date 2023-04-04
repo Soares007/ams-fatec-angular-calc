825a4e6789e6155aa8fca429d764d7a16265ba96
Soares007 <soaresstar947@gmail.com>
2023-04-04 17:22:50 -0300

    Calc-versao-1

---
+++ src/app/calc/calc.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calc',
+  templateUrl: './calc.component.html',
+  styleUrls: ['./calc.component.css']
+})
+export class CalcComponent {
+  num1: number = 0;
+  num2: number = 0;
+  result: number = 0;
+
+
+somar (){
+  this.result= this.num1 + this.num2;
+}
+subtrair (){
+ this.result=this.num1 - this.num2;
+}
+multiplicar () {
+  this.result=this.num1 * this.num2;
+}
+dividir (){
+  this.result=this.num1 / this.num2;
+}
+}
